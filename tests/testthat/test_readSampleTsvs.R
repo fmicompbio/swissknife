59eb968fb38b877408690878f42f047e9f18c589
Charlotte Soneson <charlottesoneson@gmail.com>
2018-11-14 14:12:27 +0100

    Add more unit tests

--- tests/testthat/test_readSampleTsvs.R
+++ tests/testthat/test_readSampleTsvs.R
@@ -15,7 +15,19 @@ test_that("readSampleTsvs() works properly", {
                             TSV_FILE = "readSampleTsvsEx1.tsv",
                             stringsAsFactors = FALSE))
     
-    ## Check that samples with multiple matches will not be read if keepMulti=FALSE
+    ## Check that the output is as expected for multiple existing files
+    expect_equal(readSampleTsvs(seqdataDir = seqdataDir,
+                                sampleIds = c("readSampleTsvsEx1",
+                                              "readSampleTsvsEx2")),
+                 data.frame(SAMPLE_ID = c("readSampleTsvsEx1",
+                                          "readSampleTsvsEx2"),
+                            FIELD1 = c("value1", "value3"),
+                            FIELD2 = c("value2", "value4"),
+                            TSV_FILE = c("readSampleTsvsEx1.tsv",
+                                         "readSampleTsvsEx2.tsv"),
+                            stringsAsFactors = FALSE))
+    
+    ## Check that samples with multiple matches will not be kept if keepMulti=FALSE
     expect_warning(df <- readSampleTsvs(seqdataDir = seqdataDir, 
                                         sampleIds = "readSampleTsvsEx",
                                         keepMulti = FALSE))
@@ -29,4 +41,5 @@ test_that("readSampleTsvs() works properly", {
     expect_equal(nrow(df), 3)
     expect_equal(ncol(df), 4)
     expect_equal(df$SAMPLE_ID, rep("readSampleTsvsEx", 3))
+    expect_equal(sort(df$FIELD1), sort(c("value1", "value3", "value5")))
 })

290aa9dd753463e24568a83719c32513769499dd
mbstadler <stadler.michael@gmail.com>
2018-11-16 14:19:22 +0100

    add a test to capture the problem

--- tests/testthat/test_sampleControlElements.R
+++ tests/testthat/test_sampleControlElements.R
@@ -4,14 +4,19 @@ test_that("sampleControlElements() works properly", {
     x <- c(runif(length(i.control), min = 0, max = 10),
            rnorm(length(i.target), mean = 5, sd = 1))
     set.seed(1)
-    s1 <- sampleControlElements(x, idxTarget = i.target, idxControl = i.control)
+    expect_true(is.numeric(s1 <- sampleControlElements(x, idxTarget = i.target, idxControl = i.control)))
     set.seed(1)
-    s2 <- sampleControlElements(x,
-                                idxTarget = rep(c(FALSE,TRUE),c(length(i.control),length(i.target))),
-                                idxControl = NULL)
+    expect_true(is.numeric(s2 <- sampleControlElements(x,
+                                      idxTarget = rep(c(FALSE,TRUE),c(length(i.control),length(i.target))),
+                                      idxControl = NULL)))
+    set.seed(1)
+    expect_true(is.numeric(s3 <- sampleControlElements(list(x),
+                                      idxTarget = rep(c(FALSE,TRUE),c(length(i.control),length(i.target))),
+                                      idxControl = i.control)))
 
     expect_equal(length(s1), length(i.target))
     expect_equal(s1, s2)
+    expect_equal(s1, s3)
     expect_true(all(s1 %in% i.control))
     expect_equal(quantile(x[s1]), quantile(x[i.target]), tolerance = 0.1)
     expect_error(sampleControlElements(x, idxTarget = -1))

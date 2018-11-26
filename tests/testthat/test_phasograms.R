ea344a76fbc58b4b9edddf6984425995beeea24b
mbstadler <stadler.michael@gmail.com>
2018-11-26 15:19:53 +0100

    additional tests for complete code coverage

--- tests/testthat/test_phasograms.R
+++ tests/testthat/test_phasograms.R
@@ -1,10 +1,25 @@
 test_that("calcPhasogram(), estimateNRL() and calcAndCountDist() work properly", {
+    ## create phasograms using MNase data from chr10:20000000-21000000
     bamf <- system.file("extdata", "phasograms", "mnase_mm10.bam", package = "swissknife")
     pg1 <- calcPhasogram(bamf)
     pg2 <- calcPhasogram(bamf, rmdup = FALSE)
     pg3 <- calcPhasogram(c(bamf,bamf))
     pg4 <- calcPhasogram(bamf, regions = GenomicRanges::GRanges("chr10", IRanges::IRanges(start = 19000000, end = 23000000)))
     pg5 <- calcPhasogram(bamf, regions = GenomicRanges::GRanges("chr11", IRanges::IRanges(start = 19000000, end = 23000000)))
+
+    ## check invalid arguments
+    expect_warning(estimateNRL(rep(0,3000L))) # all zeros
+    expect_warning(estimateNRL(pg1, usePeaks = 1:100)) # too many usePeaks
+
+    ## check if the plotting function runs
+    tf <- tempfile(fileext = ".pdf")
+    pdf(file = tf)
+    expect_true(is.list(plotPhasogram(x = pg1)))
+    expect_true(is.list(plotPhasogram(x = pg1, verbosePlot = TRUE)))
+    dev.off()
+    unlink(tf)
+
+    ## check expected results
     expect_true(is.numeric(pg1))
     expect_length(pg1, 3000L)
     expect_true(all(pg2 > pg1))

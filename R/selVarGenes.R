88e8b76464ee4beb4ac8c0ada812f69e071a9f0c
machlabd <dania.machlab@gmail.com>
2019-12-20 11:58:28 +0100

    documented functions more

--- R/selVarGenes.R
+++ R/selVarGenes.R
@@ -39,132 +39,164 @@
 #' @export
 #' 
 getDistMat <- function(loessModel=NULL, x=NULL, y=NULL, x_curve=NULL, method="euclidean", ...){
-  
-  ## checks
-  stopifnot(!is.null(x_curve))
-  stopifnot(!is.null(x))
-  stopifnot(!is.null(y))
-  stopifnot(!is.null(loessModel))
-  stopifnot(length(x)==length(y))
-  stopifnot(is.numeric(x_curve))
-  stopifnot(is.numeric(x))
-  stopifnot(is.numeric(y))
-  
-  ## predict y_curv for x_curve using loessModel
-  y_curve <- predict(loessModel, newdata = x_curve)
-  
-  ## get distances
-  distMat <- wordspace::dist.matrix(M = cbind(x,y), M2 = cbind(x_curve, y_curve), method = method, ... )
-  colnames(distMat) <- paste0("pointOnCurve", 1:length(y_curve))
-  if(!is.null(names(x))){
-    rownames(distMat) <- names(x)
-  } else {
-    rownames(distMat) <- paste0("xy", 1:length(x))
-  }
-  
-  ## return
-  distMat
-  
+     
+     ## checks
+     stopifnot(!is.null(x_curve))
+     stopifnot(!is.null(x))
+     stopifnot(!is.null(y))
+     stopifnot(!is.null(loessModel))
+     stopifnot(length(x)==length(y))
+     stopifnot(is.numeric(x_curve))
+     stopifnot(is.numeric(x))
+     stopifnot(is.numeric(y))
+     
+     ## predict y_curv for x_curve using loessModel
+     y_curve <- predict(loessModel, newdata = x_curve)
+     
+     ## get distances
+     distMat <- wordspace::dist.matrix(M = cbind(x,y), M2 = cbind(x_curve, y_curve), method = method, ... )
+     colnames(distMat) <- paste0("pointOnCurve", 1:length(y_curve))
+     if(!is.null(names(x))){
+          rownames(distMat) <- names(x)
+     } else {
+          rownames(distMat) <- paste0("xy", 1:length(x))
+     }
+     
+     ## return
+     distMat
+     
 }
 
 
 #' @title Select Variable Genes in Single Cell RNA-seq
 #' 
 #' @description This function selects the most variable genes from a \code{Single Cell Experiment} object
-#'    using the plot that displays the log2 coefficient of variation as a function of the log2 mean for 
-#'    all genes across all the cells.
+#'   using the plot that displays the log2 coefficient of variation as a function of the log2 mean for 
+#'   all genes across all the cells.
 #' 
 #' @author Dania Machlab 
 #' 
-#' @param sce \code{Single Cell Experiment} object containing the raw counts and the scaling factors.
+#' @param sce \code{SingleCellExperiment} object containing the raw counts and the scaling factors.
 #' @param Nmads number of MADs beyond which genes are selected per bin.
 #' @param minCells keep genes with minimum expression in at least this number of cells.
 #' @param minExpr keep genes with expression greater than or equal to this in \code{minCells} cells using 
 #'    the raw count matrix corrected for library size differences using the sizeFactors from \code{sce}.
-#' @param topExprPerc
-#' @param span
-#' @param family 
-#' @param control
-#' @param binBreaks
-#' @param accurateDistBreaks
+#' @param topExprPerc the top percent expressed genes that will be excluded from the loess fit. 
+#' @param span span parameter for \code{loess} function.
+#' @param control control parameters for \code{loess} function.
+#' @param binBreaks number of bins or groups to place the points(genes) into.
+#' @param accurateDistBreaks number of bins or groups to use  to place the points(genes) into
+#'    when calculating more accurate distance values to the curve from the loess fit.
 #' @param ... additional parameters for the \code{loess} function from the \code{stats} package.
 #'
-#' @details 
+#' @details The function takes in a \code{SingleCellExperiment} object and calculates the normalized
+#'   counts by dividing the raw counts by the corresponding sizeFactors per cell. Only genes that have
+#'   an expression greater than or equal to \code{minExpr} in at least \code{minCells} cells will be
+#'   kept.
+#'   
+#'   The genes that vary most on the log2(coefficient of variation) vs log2(mean) plot of genes will 
+#'   be selected. A loess fit is done on this plot and the distance (euclidean by default) each point has
+#'   to the curve is calculated in two steps.
+#'   
+#'   In the first step, genes are assigned to bins by taking the minimum distance to the curve. By default
+#'   we select 100 points on the loess fit and calculate the distances each gene has to all those points on 
+#'   the curve. Each gene is assigned to the point on the curve for which it has the shortest distance. In 
+#'   the second step, more accurate distances to the curve are calculated by using a higher number of 
+#'   points on the curve.
+#'   
+#'   Finally, for each bin, the most variable genes are selected using the more accurate distance
+#'   measures. Genes that fall below the loess fit are assigned a negative sign and the genes that 
+#'   are \code{Nmads} MADs away from the median are selected.
+#'   
 #' 
-#' @return 
-#'
+#' @return a \code{list} of length 2:
+#'   \itemize{
+#'      \item varGenes: \code{vector} containing the names of the most variable genes.
+#'      \item geneInfo: \code{data.frame} with genes as rows and columns containing  
+#'                      calculated measures:
+#'                      \itemize{
+#'                         \item logMean: log2(mean) expression of genes across cells.
+#'                         \item logCV: log2(coefficient of variation) of genes across cells.
+#'                         \item pred_logCV: predicted log2(coefficient of variation) from loess fit.
+#'                         \item bin_per_gene: bin each gene has been assigned to.
+#'                         \item min_dist1_per_gene: first round of calcultaing distances.
+#'                         \item min_dist2_per_gene: second round of calcultaing more accurate distances
+#'                      }
+#'   }
+#'   
 #' @examples 
 #'
 #' @importFrom stats loess predict mad median
 #' 
 #' @export
 #' 
-selVarGenes <- function(sce, Nmads = 3, minCells = 5, minExpr = 1, topExprPerc = 0.01, span = 0.2, 
-                        family = "symmetric", control=loess.control(surface = "direct"), binBreaks=100, 
+selVarGenes <- function(sce=NULL, Nmads = 3, minCells = 5, minExpr = 1, topExprPerc = 0.01, span = 0.2, 
+                        control=loess.control(surface = "direct"), binBreaks=100, 
                         accurateDistBreaks = ceiling(nrow(sce)/4), ...){
-  
-  ## checks
-  if(class(sce)!="SingleCellExperiment"){stop("sce must be a SingleCellExperiment object")}
-  if(isEmpty(sizeFactors(sce))){stop("sce must contain sizeFactors to normalize the raw counts")}
-  
-  ## normCounts
-  normCounts <- sweep(as.matrix(counts(sce)), 2, sizeFactors(sce), "/")
-  if(is.null(rownames(normCounts))){
-    rownames(normCounts) <- paste0("Gene_", 1:nrow(normCounts))
-  }
-  if(is.null(colnames(normCounts))){
-    colnames(normCounts) <- paste0("Cell_", 1:ncol(normCounts))
-  }
-  
-  ## keep genes with minExpression in at least minCells
-  keep <- rowSums(normCounts>=minExpr) >= minCells
-  normCounts <- normCounts[keep, ]
-  
-  ## log2(mean expression) and log2(coefficient of variaition)
-  logMean <- log2(rowMeans(normCounts))
-  logCV <- log2(apply(normCounts, 1, sd)/rowMeans(normCounts))
-  df <- data.frame(logMean = logMean, logCV = logCV)
-  
-  ## loess fit, excluding the top topExprPerc
-  points_for_loess <- which(logMean < quantile(logMean, (1-topExprPerc)))
-  lo <- stats::loess(logCV ~ logMean, span = span, family = family, control = control, subset = points_for_loess, ...)
-  df$pred_logCV <- stats::predict(lo, newdata = df$logMean)
-  
-  ## assign to groups (min eucl distance to points on the loess curve)
-  dist1 <- getDistMat(loessModel=lo, x=logMean, y=logCV, x_curve=seq(from = range(logMean)[1], to = range(logMean)[2], length.out = binBreaks))
-  df$bin_per_gene <- apply(X = dist1, MARGIN = 1, FUN = function(x){which.min(x)}) 
-  df$min_dist1_per_gene <- sapply(seq_along(df$bin_per_gene), function(i){dist1[i, df$bin_per_gene[i]]})
-  
-  ## get more accurate eucl dist to the curve for each gene
-  dist2 <- getDistMat(loessModel=lo, x=logMean, y=logCV, x_curve=seq(from = range(logMean)[1], to = range(logMean)[2], length.out = accurateDistBreaks))
-  sel <- apply(X = dist2, MARGIN = 1, FUN = function(x){which.min(x)}) 
-  df$min_dist2_per_gene <- sapply(seq_along(sel), function(i){dist2[i, sel[i]]})
-  
-  ## give dist2 a sign: + for above the curve, and - for below the curve
-  y_pred_for_x <- stats::predict(lo, newdata = logMean)
-  w_down <- logCV < y_pred_for_x
-  df$min_dist2_per_gene[w_down] <- -df$min_dist2_per_gene[w_down]
-  
-  ## genes per group
-  genes_per_bin <- lapply(unique(df$bin_per_gene), function(i){rownames(df)[df$bin_per_gene==i]}) # assuming df has rownames!
-  names(genes_per_bin) <- unique(df$bin_per_gene)
-  
-  ## MAD (this includes genes that were excluded from the loess) per group
-  mads <- sapply(genes_per_bin, function(x){stats::mad(df[x, ]$min_dist2_per_gene)})
-  mads <- mads*Nmads
-  
-  ## select outlier genes per group
-  out_genes <- unlist(lapply(seq_along(genes_per_bin), function(i){
-    genes <- genes_per_bin[[i]]
-    distances <- df[genes, ]$min_dist2_per_gene
-    median <- stats::median(distances)
-    genes[distances > (median + mads[i])]
-  }))
-  out_genes <- out_genes[!is.na(out_genes)]
-  
-  ## return outlier genes
-  list(varGenes=out_genes, geneInfo=df)
-  
+     
+     ## checks
+     if(is.null(sce)){stop("'sce' is empty")}
+     if(class(sce)!="SingleCellExperiment"){stop("sce must be a SingleCellExperiment object")}
+     if(isEmpty(sizeFactors(sce))){stop("sce must contain sizeFactors to normalize the raw counts")}
+     
+     ## normCounts
+     normCounts <- sweep(as.matrix(counts(sce)), 2, sizeFactors(sce), "/")
+     if(is.null(rownames(normCounts))){
+          rownames(normCounts) <- paste0("Gene_", 1:nrow(normCounts))
+     }
+     if(is.null(colnames(normCounts))){
+          colnames(normCounts) <- paste0("Cell_", 1:ncol(normCounts))
+     }
+     
+     ## keep genes with minExpression in at least minCells
+     keep <- rowSums(normCounts>=minExpr) >= minCells
+     normCounts <- normCounts[keep, ]
+     
+     ## log2(mean expression) and log2(coefficient of variaition)
+     logMean <- log2(rowMeans(normCounts))
+     logCV <- log2(apply(normCounts, 1, sd)/rowMeans(normCounts))
+     df <- data.frame(logMean = logMean, logCV = logCV)
+     
+     ## loess fit, excluding the top topExprPerc
+     points_for_loess <- which(logMean < quantile(logMean, (1-topExprPerc)))
+     lo <- stats::loess(logCV ~ logMean, span = span, control = control, subset = points_for_loess, ...)
+     df$pred_logCV <- stats::predict(lo, newdata = df$logMean)
+     
+     ## assign to groups (min eucl distance to points on the loess curve)
+     dist1 <- getDistMat(loessModel=lo, x=logMean, y=logCV, x_curve=seq(from = range(logMean)[1], to = range(logMean)[2], length.out = binBreaks))
+     df$bin_per_gene <- apply(X = dist1, MARGIN = 1, FUN = function(x){which.min(x)}) 
+     df$min_dist1_per_gene <- sapply(seq_along(df$bin_per_gene), function(i){dist1[i, df$bin_per_gene[i]]})
+     
+     ## get more accurate eucl dist to the curve for each gene
+     dist2 <- getDistMat(loessModel=lo, x=logMean, y=logCV, x_curve=seq(from = range(logMean)[1], to = range(logMean)[2], length.out = accurateDistBreaks))
+     sel <- apply(X = dist2, MARGIN = 1, FUN = function(x){which.min(x)}) 
+     df$min_dist2_per_gene <- sapply(seq_along(sel), function(i){dist2[i, sel[i]]})
+     
+     ## give dist2 a sign: + for above the curve, and - for below the curve
+     y_pred_for_x <- stats::predict(lo, newdata = logMean)
+     w_down <- logCV < y_pred_for_x
+     df$min_dist2_per_gene[w_down] <- -df$min_dist2_per_gene[w_down]
+     
+     ## genes per group
+     genes_per_bin <- lapply(unique(df$bin_per_gene), function(i){rownames(df)[df$bin_per_gene==i]}) # assuming df has rownames!
+     names(genes_per_bin) <- unique(df$bin_per_gene)
+     
+     ## MAD (this includes genes that were excluded from the loess) per group
+     mads <- sapply(genes_per_bin, function(x){stats::mad(df[x, ]$min_dist2_per_gene)})
+     mads <- mads*Nmads
+     
+     ## select outlier genes per group
+     out_genes <- unlist(lapply(seq_along(genes_per_bin), function(i){
+          genes <- genes_per_bin[[i]]
+          distances <- df[genes, ]$min_dist2_per_gene
+          median <- stats::median(distances)
+          genes[distances > (median + mads[i])]
+     }))
+     out_genes <- out_genes[!is.na(out_genes)]
+     
+     ## return outlier genes
+     list(varGenes=out_genes, geneInfo=df)
+     
 }
 
 
@@ -172,8 +204,8 @@ selVarGenes <- function(sce, Nmads = 3, minCells = 5, minExpr = 1, topExprPerc =
 #' @title Plot Groups Genes Have Been Assigned to
 #' 
 #' @description This function take the output from \code{selVarGenes} and plots the group or bin each
-#'    gene has been assigned to on the scatter plot showing the log2 coefficient of variation as a
-#'    function of the log2 mean.
+#'   gene has been assigned to on the scatter plot showing the log2 coefficient of variation as a
+#'   function of the log2 mean.
 #' 
 #' @author Dania Machlab 
 #' 
@@ -197,31 +229,31 @@ selVarGenes <- function(sce, Nmads = 3, minCells = 5, minExpr = 1, topExprPerc =
 plotSelVarGenesGroups <- function(selVarGenes_list = NULL, xlab="logMean", 
                                   ylab="logCV", main = "Group Assignment per Gene", 
                                   asp=1, ...) {
-  
-  ## checks
-  if(any(isEmpty(selVarGenes_list))){
-      stop("'selVarGenes_list' is empty")
-    }
-  if(class(selVarGenes_list)!="list"){
-      stop("'selVarGenes_list' must be of class 'list'")
-    }
-  if(!all(names(selVarGenes_list)%in%c("varGenes", "geneInfo"))){
-      stop("names of 'selVarGenes_list' must be 'varGenes' and 'geneInfo', 
+     
+     ## checks
+     if(any(isEmpty(selVarGenes_list))){
+          stop("'selVarGenes_list' is empty")
+     }
+     if(class(selVarGenes_list)!="list"){
+          stop("'selVarGenes_list' must be of class 'list'")
+     }
+     if(!all(names(selVarGenes_list)%in%c("varGenes", "geneInfo"))){
+          stop("names of 'selVarGenes_list' must be 'varGenes' and 'geneInfo', 
            the output from the 'selVarGenes' function")
-    }
-  
-  ## prepare for plot
-  colors <- grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert=TRUE)]
-  yHat_cols <- sample(colors, length(unique(selVarGenes_list$geneInfo$bin_per_gene)))
-  cols <- yHat_cols[selVarGenes_list$geneInfo$bin_per_gene]
-  
-  ## plot
-  plot(selVarGenes_list$geneInfo$logMean, selVarGenes_list$geneInfo$logCV, bg=cols, pch=21, 
-       asp=asp, xlab=xlab, ylab=ylab, main=main, ...)
-  
-  ## return TRUE
-  invisible(TRUE)
-
+     }
+     
+     ## prepare for plot
+     colors <- grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert=TRUE)]
+     yHat_cols <- sample(colors, length(unique(selVarGenes_list$geneInfo$bin_per_gene)))
+     cols <- yHat_cols[selVarGenes_list$geneInfo$bin_per_gene]
+     
+     ## plot
+     plot(selVarGenes_list$geneInfo$logMean, selVarGenes_list$geneInfo$logCV, bg=cols, pch=21, 
+          asp=asp, xlab=xlab, ylab=ylab, main=main, ...)
+     
+     ## return TRUE
+     invisible(TRUE)
+     
 }
 
 
@@ -230,8 +262,8 @@ plotSelVarGenesGroups <- function(selVarGenes_list = NULL, xlab="logMean",
 #' @title Plot Selected Variable Genes
 #' 
 #' @description This function take the output from \code{selVarGenes} and plots the genes that have been 
-#'    selected to be highly variable across the cells. It plot the log2 coefficient of variation as
-#'    a function of the log mean.
+#'   selected to be highly variable across the cells. It plot the log2 coefficient of variation as
+#'   a function of the log mean.
 #' 
 #' @author Dania Machlab 
 #' 
@@ -253,33 +285,33 @@ plotSelVarGenesGroups <- function(selVarGenes_list = NULL, xlab="logMean",
 #' @export
 #' 
 plotSelVarGenes <- function(selVarGenes_list = NULL, xlab="logMean", 
-                                  ylab="logCV", main = "Selected Variable Genes", 
-                                  pch=16, col="#BEBEBE40", sel_col="steelblue", ...) {
-  
-  ## checks
-  if(any(isEmpty(selVarGenes_list))){
-    stop("'selVarGenes_list' is empty")
-  }
-  if(class(selVarGenes_list)!="list"){
-    stop("'selVarGenes_list' must be of class 'list'")
-  }
-  if(!all(names(selVarGenes_list)%in%c("varGenes", "geneInfo"))){
-    stop("names of 'selVarGenes_list' must be 'varGenes' and 'geneInfo', 
+                            ylab="logCV", main = "Selected Variable Genes", 
+                            pch=16, col="#BEBEBE40", sel_col="steelblue", ...) {
+     
+     ## checks
+     if(any(isEmpty(selVarGenes_list))){
+          stop("'selVarGenes_list' is empty")
+     }
+     if(class(selVarGenes_list)!="list"){
+          stop("'selVarGenes_list' must be of class 'list'")
+     }
+     if(!all(names(selVarGenes_list)%in%c("varGenes", "geneInfo"))){
+          stop("names of 'selVarGenes_list' must be 'varGenes' and 'geneInfo', 
            the output from the 'selVarGenes' function")
-  }
-
-  ## plot
-  plot(selVarGenes_list$geneInfo$logMean, selVarGenes_list$geneInfo$logCV, 
-       xlab=xlab, ylab=ylab, main=main, pch=pch, col=col, ...)
-  points(selVarGenes_list$geneInfo[selVarGenes_list$varGenes, ]$logMean, selVarGenes_list$geneInfo[selVarGenes_list$varGenes, ]$logCV, 
-         pch=pch, col=sel_col)
-  lines(df$logMean[order(df$logMean)], df$pred_logCV[order(df$logMean)], col="red")
-  legend("bottomleft", bty="n", col=c(sel_col, "red"), pch=c(pch, NA), 
-         lty=c(NA, 1), legend=c("SelVarGene", "loess fit"))
-  
-  ## return TRUE
-  invisible(TRUE)
-  
+     }
+     
+     ## plot
+     plot(selVarGenes_list$geneInfo$logMean, selVarGenes_list$geneInfo$logCV, 
+          xlab=xlab, ylab=ylab, main=main, pch=pch, col=col, ...)
+     points(selVarGenes_list$geneInfo[selVarGenes_list$varGenes, ]$logMean, selVarGenes_list$geneInfo[selVarGenes_list$varGenes, ]$logCV, 
+            pch=pch, col=sel_col)
+     lines(df$logMean[order(df$logMean)], df$pred_logCV[order(df$logMean)], col="red")
+     legend("bottomleft", bty="n", col=c(sel_col, "red"), pch=c(pch, NA), 
+            lty=c(NA, 1), legend=c("SelVarGene", "loess fit"))
+     
+     ## return TRUE
+     invisible(TRUE)
+     
 }
 
 

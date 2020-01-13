c5b3b2959bad6d0689770201a749c940c23b407c
machlabd <dania.machlab@gmail.com>
2020-01-13 09:42:27 +0100

    fixed spacing

--- R/selVarGenes.R
+++ R/selVarGenes.R
@@ -39,32 +39,32 @@
 #' @keywords internal
 #' 
 .getDistMat <- function(loessModel = NULL, x = NULL, y = NULL, x_curve = NULL, method = "euclidean", ...){
-     
-     ## checks
-     stopifnot(!is.null(x_curve))
-     stopifnot(!is.null(x))
-     stopifnot(!is.null(y))
-     stopifnot(!is.null(loessModel))
-     stopifnot(length(x) == length(y))
-     stopifnot(is.numeric(x_curve))
-     stopifnot(is.numeric(x))
-     stopifnot(is.numeric(y))
-
-     ## predict y_curve for x_curve using loessModel
-     y_curve <- stats::predict(loessModel, newdata = x_curve)
-     
-     ## get distances
-     distMat <- wordspace::dist.matrix(M = cbind(x, y), M2 = cbind(x_curve, y_curve), method = method, ... )
-     colnames(distMat) <- paste0("pointOnCurve", seq_len(length(y_curve)))
-     if (!is.null(names(x))) {
-          rownames(distMat) <- names(x)
-     } else {
-          rownames(distMat) <- paste0("xy", seq_len(length(x)))
-     }
-     
-     ## return
-     distMat
-     
+    
+    ## checks
+    stopifnot(!is.null(x_curve))
+    stopifnot(!is.null(x))
+    stopifnot(!is.null(y))
+    stopifnot(!is.null(loessModel))
+    stopifnot(length(x) == length(y))
+    stopifnot(is.numeric(x_curve))
+    stopifnot(is.numeric(x))
+    stopifnot(is.numeric(y))
+    
+    ## predict y_curve for x_curve using loessModel
+    y_curve <- stats::predict(loessModel, newdata = x_curve)
+    
+    ## get distances
+    distMat <- wordspace::dist.matrix(M = cbind(x, y), M2 = cbind(x_curve, y_curve), method = method, ... )
+    colnames(distMat) <- paste0("pointOnCurve", seq_len(length(y_curve)))
+    if (!is.null(names(x))) {
+        rownames(distMat) <- names(x)
+    } else {
+        rownames(distMat) <- paste0("xy", seq_len(length(x)))
+    }
+    
+    ## return
+    distMat
+    
 }
 
 
@@ -166,106 +166,106 @@
 selVarGenes <- function(data=NULL, assay.type="counts", logPseudo = 1, Nmads = 3, minCells = 5, minExpr = 1, exclTopExprFrac = 0.01, span = 0.2, 
                         control=stats::loess.control(surface = "direct"), nBins = 100, 
                         nBinsDense = ceiling(nrow(data)/4), ...){
-     
-     ## checks
-     if (is.null(data)) {stop("'data' is empty")}
-     if (!is(data, "SingleCellExperiment") & !is(data, "matrix")) {stop("data must be a SingleCellExperiment object or a count matrix")}
-     if (is(data, "SingleCellExperiment") & !any(assay.type%in%c("counts", "logcounts"))) {stop("assay.type must be either counts or logcounts")}
-     if (is(data, "SingleCellExperiment") & assay.type=="counts") {
-          if(!any(names(SummarizedExperiment::assays(data)) %in% "counts")) {stop("counts assay is missing from SCE object")}
-     }
-     if (is(data, "SingleCellExperiment") & assay.type=="logcounts") {
-          if(!any(names(SummarizedExperiment::assays(data)) %in% "logcounts")) {stop("logcounts assay is missing from SCE object")}
-     }
-     if (is(data, "SingleCellExperiment") & assay.type=="counts") {
-          if(is.null(SingleCellExperiment::sizeFactors(data))) {stop("missing sizeFactors to normalize counts")}
-     }
+    
+    ## checks
+    if (is.null(data)) {stop("'data' is empty")}
+    if (!is(data, "SingleCellExperiment") & !is(data, "matrix")) {stop("data must be a SingleCellExperiment object or a count matrix")}
+    if (is(data, "SingleCellExperiment") & !any(assay.type%in%c("counts", "logcounts"))) {stop("assay.type must be either counts or logcounts")}
+    if (is(data, "SingleCellExperiment") & assay.type=="counts") {
+        if(!any(names(SummarizedExperiment::assays(data)) %in% "counts")) {stop("counts assay is missing from SCE object")}
+    }
+    if (is(data, "SingleCellExperiment") & assay.type=="logcounts") {
+        if(!any(names(SummarizedExperiment::assays(data)) %in% "logcounts")) {stop("logcounts assay is missing from SCE object")}
+    }
+    if (is(data, "SingleCellExperiment") & assay.type=="counts") {
+        if(is.null(SingleCellExperiment::sizeFactors(data))) {stop("missing sizeFactors to normalize counts")}
+    }
+    
+    ## when data is SCE
+    if(is(data, "SingleCellExperiment")) {
+        if(assay.type=="logcounts"){
+            message("using logcounts(data), assuming those counts are in the log2(raw norm counts + 1) space ...")
+            normCounts <- 2^SingleCellExperiment::logcounts(data)-logPseudo
+        } else {
+            normCounts <- sweep(as.matrix(SingleCellExperiment::counts(data)), 2, SingleCellExperiment::sizeFactors(data), "/")  
+        }
         
-     ## when data is SCE
-     if(is(data, "SingleCellExperiment")) {
-          if(assay.type=="logcounts"){
-               message("using logcounts(data), assuming those counts are in the log2(raw norm counts + 1) space ...")
-               normCounts <- 2^SingleCellExperiment::logcounts(data)-logPseudo
-          } else {
-               normCounts <- sweep(as.matrix(SingleCellExperiment::counts(data)), 2, SingleCellExperiment::sizeFactors(data), "/")  
-          }
-             
-     } 
-     
-     ## when data is count matrix
-     if(is(data, "matrix")){
-          message("assuming that data is a normalized count matrix with genes as rows and cells as columns, if that's not the case make sure it is ... ")
-          normCounts <- data
-     }
-        
-     ## normCounts rownames and colnames
-     if (is.null(rownames(normCounts))) {
-          rownames(normCounts) <- paste0("Gene_", seq_len(nrow(normCounts)))
-          message("Count row names are empty, naming them now ... ")
-     }
-     if (is.null(colnames(normCounts))) {
-          colnames(normCounts) <- paste0("Cell_", seq_len(ncol(normCounts)))
-          message("Count column names are empty, naming them now ... ")
-     }
-     
-     ## keep genes with minExpression in at least minCells
-     keep <- rowSums(normCounts >= minExpr) >= minCells
-     normCounts <- normCounts[keep, , drop = FALSE]
-     
-     ## log2(mean expression) and log2(coefficient of variation)
-     logMean <- log2(rowMeans(normCounts))
-     logCV <- log2(apply(normCounts, 1, stats::sd)/rowMeans(normCounts))
-     datfr <- data.frame(logMean = logMean, logCV = logCV)
-     
-     ## loess fit, excluding the top exclTopExprFrac
-     points_for_loess <- which(logMean < stats::quantile(logMean, (1 - exclTopExprFrac)))
-     lo <- stats::loess(logCV ~ logMean, span = span, control = control, 
-                        subset = points_for_loess, ...)
-     datfr$pred_logCV <- stats::predict(lo, newdata = datfr$logMean)
-     
-     ## assign to groups (min eucl distance to points on the loess curve)
-     dist1 <- .getDistMat(loessModel = lo, x = logMean, y = logCV, 
-                          x_curve = seq(from = range(logMean)[1], 
-                                        to = range(logMean)[2], 
-                                        length.out = nBins))
-     datfr$assigned_bin <- apply(X = dist1, MARGIN = 1, FUN = function(x){which.min(x)}) 
-     # datfr$min_dist1_per_gene <- sapply(seq_along(datfr$assigned_bin), 
-     #                                    function(i){dist1[i, datfr$assigned_bin[i]]})
-      
-     ## get more accurate eucl dist to the curve for each gene
-     dist2 <- .getDistMat(loessModel = lo, x = logMean, y = logCV, 
+    } 
+    
+    ## when data is count matrix
+    if(is(data, "matrix")){
+        message("assuming that data is a normalized count matrix with genes as rows and cells as columns, if that's not the case make sure it is ... ")
+        normCounts <- data
+    }
+    
+    ## normCounts rownames and colnames
+    if (is.null(rownames(normCounts))) {
+        rownames(normCounts) <- paste0("Gene_", seq_len(nrow(normCounts)))
+        message("Count row names are empty, naming them now ... ")
+    }
+    if (is.null(colnames(normCounts))) {
+        colnames(normCounts) <- paste0("Cell_", seq_len(ncol(normCounts)))
+        message("Count column names are empty, naming them now ... ")
+    }
+    
+    ## keep genes with minExpression in at least minCells
+    keep <- rowSums(normCounts >= minExpr) >= minCells
+    normCounts <- normCounts[keep, , drop = FALSE]
+    
+    ## log2(mean expression) and log2(coefficient of variation)
+    logMean <- log2(rowMeans(normCounts))
+    logCV <- log2(apply(normCounts, 1, stats::sd)/rowMeans(normCounts))
+    datfr <- data.frame(logMean = logMean, logCV = logCV)
+    
+    ## loess fit, excluding the top exclTopExprFrac
+    points_for_loess <- which(logMean < stats::quantile(logMean, (1 - exclTopExprFrac)))
+    lo <- stats::loess(logCV ~ logMean, span = span, control = control, 
+                       subset = points_for_loess, ...)
+    datfr$pred_logCV <- stats::predict(lo, newdata = datfr$logMean)
+    
+    ## assign to groups (min eucl distance to points on the loess curve)
+    dist1 <- .getDistMat(loessModel = lo, x = logMean, y = logCV, 
+                         x_curve = seq(from = range(logMean)[1], 
+                                       to = range(logMean)[2], 
+                                       length.out = nBins))
+    datfr$assigned_bin <- apply(X = dist1, MARGIN = 1, FUN = function(x){which.min(x)}) 
+    # datfr$min_dist1_per_gene <- sapply(seq_along(datfr$assigned_bin), 
+    #                                    function(i){dist1[i, datfr$assigned_bin[i]]})
+    
+    ## get more accurate eucl dist to the curve for each gene
+    dist2 <- .getDistMat(loessModel = lo, x = logMean, y = logCV, 
                          x_curve = seq(from = range(logMean)[1], 
                                        to = range(logMean)[2], 
                                        length.out = nBinsDense))
-     sel <- apply(X = dist2, MARGIN = 1, FUN = function(x){which.min(x)}) 
-     datfr$distance <- sapply(seq_along(sel), function(i){dist2[i, sel[i]]})
-     
-     ## give dist2 a sign: + for above the curve, and - for below the curve
-     y_pred_for_x <- stats::predict(lo, newdata = logMean)
-     w_down <- logCV < y_pred_for_x
-     datfr$distance[w_down] <- -datfr$distance[w_down]
-     
-     ## genes per group
-     genes_per_bin <- lapply(unique(datfr$assigned_bin), 
-                             function(i){rownames(datfr)[datfr$assigned_bin == i]})
-     names(genes_per_bin) <- unique(datfr$assigned_bin)
-     
-     ## MAD (this includes genes that were excluded from the loess) per group
-     mads <- sapply(genes_per_bin, function(x){stats::mad(datfr[x, ]$distance)})
-     mads <- mads*Nmads
-     
-     ## select outlier genes per group
-     out_genes <- unlist(lapply(seq_along(genes_per_bin), function(i){
-          genes <- genes_per_bin[[i]]
-          distances <- datfr[genes, ]$distance
-          median <- stats::median(distances)
-          genes[distances > (median + mads[i])]
-     }))
-     out_genes <- out_genes[!is.na(out_genes)]
-     
-     ## return outlier genes
-     list(varGenes = out_genes, geneInfo = datfr)
-     
+    sel <- apply(X = dist2, MARGIN = 1, FUN = function(x){which.min(x)}) 
+    datfr$distance <- sapply(seq_along(sel), function(i){dist2[i, sel[i]]})
+    
+    ## give dist2 a sign: + for above the curve, and - for below the curve
+    y_pred_for_x <- stats::predict(lo, newdata = logMean)
+    w_down <- logCV < y_pred_for_x
+    datfr$distance[w_down] <- -datfr$distance[w_down]
+    
+    ## genes per group
+    genes_per_bin <- lapply(unique(datfr$assigned_bin), 
+                            function(i){rownames(datfr)[datfr$assigned_bin == i]})
+    names(genes_per_bin) <- unique(datfr$assigned_bin)
+    
+    ## MAD (this includes genes that were excluded from the loess) per group
+    mads <- sapply(genes_per_bin, function(x){stats::mad(datfr[x, ]$distance)})
+    mads <- mads*Nmads
+    
+    ## select outlier genes per group
+    out_genes <- unlist(lapply(seq_along(genes_per_bin), function(i){
+        genes <- genes_per_bin[[i]]
+        distances <- datfr[genes, ]$distance
+        median <- stats::median(distances)
+        genes[distances > (median + mads[i])]
+    }))
+    out_genes <- out_genes[!is.na(out_genes)]
+    
+    ## return outlier genes
+    list(varGenes = out_genes, geneInfo = datfr)
+    
 }
 
 
@@ -326,54 +326,53 @@ plotSelVarGenes <- function(selVarGenes_list = NULL, xlab = "logMean",
                             ylab = "logCV", main = "Selected Variable Genes", 
                             pch = 16, col = "#BEBEBE40", sel_col = "steelblue", 
                             colByBin = FALSE, asp = 1, ...) {
-     
-     ## checks
-     if (any(is.null(selVarGenes_list))) {
-          stop("'selVarGenes_list' is empty")
-     }
-     if (!is(selVarGenes_list, "list")) {
-          stop("'selVarGenes_list' must be of class 'list'")
-     }
-     if (!all(names(selVarGenes_list) %in% c("varGenes", "geneInfo"))) {
-          stop("names of 'selVarGenes_list' must be 'varGenes' and 'geneInfo', 
+    
+    ## checks
+    if (any(is.null(selVarGenes_list))) {
+        stop("'selVarGenes_list' is empty")
+    }
+    if (!is(selVarGenes_list, "list")) {
+        stop("'selVarGenes_list' must be of class 'list'")
+    }
+    if (!all(names(selVarGenes_list) %in% c("varGenes", "geneInfo"))) {
+        stop("names of 'selVarGenes_list' must be 'varGenes' and 'geneInfo', 
            the output from the 'selVarGenes' function")
-     }
-             
-     ## plot
-     if(colByBin) {
-             
-          ## prepare for plot
-          colors <- grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = TRUE)]
-          yHat_cols <- sample(colors, length(unique(selVarGenes_list$geneInfo$assigned_bin)))
-          grpCols <- yHat_cols[selVarGenes_list$geneInfo$assigned_bin]
-             
-          ## plot
-          plot(selVarGenes_list$geneInfo$logMean, selVarGenes_list$geneInfo$logCV, 
-               xlab = xlab, ylab = ylab, main = main, pch = pch, col = grpCols, 
-               asp = asp, ...)
-          
-          ## return TRUE
-          invisible(TRUE)
-             
-     } else {
+    }
+    
+    ## plot
+    if(colByBin) {
         
-          plot(selVarGenes_list$geneInfo$logMean, selVarGenes_list$geneInfo$logCV, 
-               xlab = xlab, ylab = ylab, main = main, pch = pch, col = col, ...)
-          points(selVarGenes_list$geneInfo[selVarGenes_list$varGenes, ]$logMean, 
-                 selVarGenes_list$geneInfo[selVarGenes_list$varGenes, ]$logCV, 
-                 pch = pch, col = sel_col)
-          lines(x = selVarGenes_list$geneInfo$logMean[order(selVarGenes_list$geneInfo$logMean)], 
-                y = selVarGenes_list$geneInfo$pred_logCV[order(selVarGenes_list$geneInfo$logMean)], 
-                col = "red")
-          legend("bottomleft", bty = "n", col = c(sel_col, "red"), pch = c(pch, NA), 
-                 lty = c(NA, 1), legend = c("SelVarGene", "loess fit"))
-          
-          ## return TRUE
-          invisible(TRUE)
-     
-     }
-
-     
+        ## prepare for plot
+        colors <- grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = TRUE)]
+        yHat_cols <- sample(colors, length(unique(selVarGenes_list$geneInfo$assigned_bin)))
+        grpCols <- yHat_cols[selVarGenes_list$geneInfo$assigned_bin]
+        
+        ## plot
+        plot(selVarGenes_list$geneInfo$logMean, selVarGenes_list$geneInfo$logCV, 
+             xlab = xlab, ylab = ylab, main = main, pch = pch, col = grpCols, 
+             asp = asp, ...)
+        
+        ## return TRUE
+        invisible(TRUE)
+        
+    } else {
+        
+        plot(selVarGenes_list$geneInfo$logMean, selVarGenes_list$geneInfo$logCV, 
+             xlab = xlab, ylab = ylab, main = main, pch = pch, col = col, ...)
+        points(selVarGenes_list$geneInfo[selVarGenes_list$varGenes, ]$logMean, 
+               selVarGenes_list$geneInfo[selVarGenes_list$varGenes, ]$logCV, 
+               pch = pch, col = sel_col)
+        lines(x = selVarGenes_list$geneInfo$logMean[order(selVarGenes_list$geneInfo$logMean)], 
+              y = selVarGenes_list$geneInfo$pred_logCV[order(selVarGenes_list$geneInfo$logMean)], 
+              col = "red")
+        legend("bottomleft", bty = "n", col = c(sel_col, "red"), pch = c(pch, NA), 
+               lty = c(NA, 1), legend = c("SelVarGene", "loess fit"))
+        
+        ## return TRUE
+        invisible(TRUE)
+        
+    }
+    
 }
 
 
--- tests/testthat/test_selVarGenes.R
+++ tests/testthat/test_selVarGenes.R
@@ -1,98 +1,92 @@
 context("selVarGenes")
 
 test_that(".getDistMat() works properly", {
-     
-     ## create data
-     x <- 1:1000
-     x2 <- x
-     names(x2) <- paste0("x", 1:1000)
-     y <- sort(rnorm(n = 1000, mean = 0, sd = 5)) + runif(n = 1000, min = 0, max = 10)
-     lo <- loess(y ~ x)
-     x_curve <- seq(range(x)[1], range(x)[2], length.out=100)
-     
-     ## run function
-     euclMat <- .getDistMat(loessModel=lo, x=x, y=y, x_curve=x_curve)
-     euclMat2 <- .getDistMat(loessModel=lo, x=x, y=y, x_curve=x_curve, method="manhattan")
-     euclMat3 <- .getDistMat(loessModel=lo, x=x2, y=y, x_curve=x_curve)
-     
-     ## tests
-     expect_true(length(x)==nrow(euclMat))
-     expect_true(length(x_curve)==ncol(euclMat))
-     expect_error(.getDistMat(x=x, y=y, x_curve=x_curve))
-     expect_error(.getDistMat(loessModel=lo, y=y, x_curve=x_curve))
-     expect_error(.getDistMat(loessModel=lo, x=x, x_curve=x_curve))
-     expect_error(.getDistMat(loessModel=lo, x=x, y=y))
-     expect_true(!is.null(euclMat2))
-     expect_true(all(rownames(euclMat3)==names(x2)))
-         
+    
+    ## create data
+    x <- 1:1000
+    x2 <- x
+    names(x2) <- paste0("x", 1:1000)
+    y <- sort(rnorm(n = 1000, mean = 0, sd = 5)) + runif(n = 1000, min = 0, max = 10)
+    lo <- loess(y ~ x)
+    x_curve <- seq(range(x)[1], range(x)[2], length.out=100)
+    
+    ## run function
+    euclMat <- .getDistMat(loessModel=lo, x=x, y=y, x_curve=x_curve)
+    euclMat2 <- .getDistMat(loessModel=lo, x=x, y=y, x_curve=x_curve, method="manhattan")
+    euclMat3 <- .getDistMat(loessModel=lo, x=x2, y=y, x_curve=x_curve)
+    
+    ## tests
+    expect_true(length(x)==nrow(euclMat))
+    expect_true(length(x_curve)==ncol(euclMat))
+    expect_error(.getDistMat(x=x, y=y, x_curve=x_curve))
+    expect_error(.getDistMat(loessModel=lo, y=y, x_curve=x_curve))
+    expect_error(.getDistMat(loessModel=lo, x=x, x_curve=x_curve))
+    expect_error(.getDistMat(loessModel=lo, x=x, y=y))
+    expect_true(!is.null(euclMat2))
+    expect_true(all(rownames(euclMat3)==names(x2)))
+    
 })
 
 
 test_that("selVarGenes() works properly", {
-     
-     ## create data
-     mu <- ceiling(runif(n = 2000, min = 0, max = 100))
-     counts <- do.call(rbind, lapply(mu, function(x){rpois(1000, lambda = x)}))
-     counts <- counts + 1
-     i <- sample(x = 1:nrow(counts), size = 500)
-     j <- sample(x = 1:ncol(counts), size = 500)
-     counts[i, j] <- counts[i, j] + sample(5:10, length(i), replace = TRUE)
-     sce <- SingleCellExperiment::SingleCellExperiment(list(counts=counts))
-     libsizes <- colSums(counts)
-     SingleCellExperiment::sizeFactors(sce) <- libsizes/mean(libsizes)
-     
-     SingleCellExperiment::logcounts(sce) <- log2(sweep(as.matrix(SingleCellExperiment::counts(sce)), 2, SingleCellExperiment::sizeFactors(sce), "/") + 1)
-     
-     
-     ## run function
-     sel <- selVarGenes(data = sce, assay.type = "counts")
-     sel2 <- selVarGenes(data = 2^SingleCellExperiment::logcounts(sce)-1)
-     sel3 <- selVarGenes(data = sce, assay.type = "logcounts")
-     
-     ## tests
-     expect_error(selVarGenes())
-     expect_error(selVarGenes(data = mu))
-     expect_error(selVarGenes(data = sce, assay.type = "normCounts"))
-     expect_error(selVarGenes(data = SingleCellExperiment::SingleCellExperiment(assays = list(counts=counts))))
-     expect_error(selVarGenes(data = SingleCellExperiment::SingleCellExperiment(assays = list())))
-     expect_error(selVarGenes(data = SingleCellExperiment::SingleCellExperiment(assays = list(counts=counts)), assay.type="logcounts"))
-     expect_true(all(sel$varGenes%in%rownames(sel$geneInfo)))
-     expect_true(!is.null(sel$varGenes))
-     expect_true(all(sel$varGenes==sel2$varGenes))
-     expect_true(all(sel$varGenes==sel3$varGenes))
+    
+    ## create data
+    mu <- ceiling(runif(n = 2000, min = 0, max = 100))
+    counts <- do.call(rbind, lapply(mu, function(x){rpois(1000, lambda = x)}))
+    counts <- counts + 1
+    i <- sample(x = 1:nrow(counts), size = 500)
+    j <- sample(x = 1:ncol(counts), size = 500)
+    counts[i, j] <- counts[i, j] + sample(5:10, length(i), replace = TRUE)
+    sce <- SingleCellExperiment::SingleCellExperiment(list(counts=counts))
+    libsizes <- colSums(counts)
+    SingleCellExperiment::sizeFactors(sce) <- libsizes/mean(libsizes)
+    
+    SingleCellExperiment::logcounts(sce) <- log2(sweep(as.matrix(SingleCellExperiment::counts(sce)), 2, SingleCellExperiment::sizeFactors(sce), "/") + 1)
+    
+    
+    ## run function
+    sel <- selVarGenes(data = sce, assay.type = "counts")
+    sel2 <- selVarGenes(data = 2^SingleCellExperiment::logcounts(sce)-1)
+    sel3 <- selVarGenes(data = sce, assay.type = "logcounts")
+    
+    ## tests
+    expect_error(selVarGenes())
+    expect_error(selVarGenes(data = mu))
+    expect_error(selVarGenes(data = sce, assay.type = "normCounts"))
+    expect_error(selVarGenes(data = SingleCellExperiment::SingleCellExperiment(assays = list(counts=counts))))
+    expect_error(selVarGenes(data = SingleCellExperiment::SingleCellExperiment(assays = list())))
+    expect_error(selVarGenes(data = SingleCellExperiment::SingleCellExperiment(assays = list(counts=counts)), assay.type="logcounts"))
+    expect_true(all(sel$varGenes%in%rownames(sel$geneInfo)))
+    expect_true(!is.null(sel$varGenes))
+    expect_true(all(sel$varGenes==sel2$varGenes))
+    expect_true(all(sel$varGenes==sel3$varGenes))
 })
 
 
 test_that("plotSelVarGenes() works properly", {
-     
-     ## create data
-     mu <- ceiling(runif(n = 2000, min = 0, max = 100))
-     counts <- do.call(rbind, lapply(mu, function(x){rpois(1000, lambda = x)}))
-     counts <- counts + 1
-     i <- sample(x = 1:nrow(counts), size = 500)
-     j <- sample(x = 1:ncol(counts), size = 500)
-     counts[i, j] <- counts[i, j] + sample(5:10, length(i), replace = TRUE)
-     sce <- SingleCellExperiment::SingleCellExperiment(list(counts=counts))
-     libsizes <- colSums(counts)
-     SingleCellExperiment::sizeFactors(sce) <- libsizes/mean(libsizes)
-     
-     ## run selVarGenes
-     sel <- selVarGenes(data = sce)
-     
-     ## tests
-     expect_true(plotSelVarGenes(sel))
-     expect_true(plotSelVarGenes(sel, colByBin=TRUE))
-     expect_error(plotSelVarGenes())
-     expect_error(plotSelVarGenes(mu))
-     expect_error(plotSelVarGenes(list(mu=mu)))
-     
+    
+    ## create data
+    mu <- ceiling(runif(n = 2000, min = 0, max = 100))
+    counts <- do.call(rbind, lapply(mu, function(x){rpois(1000, lambda = x)}))
+    counts <- counts + 1
+    i <- sample(x = 1:nrow(counts), size = 500)
+    j <- sample(x = 1:ncol(counts), size = 500)
+    counts[i, j] <- counts[i, j] + sample(5:10, length(i), replace = TRUE)
+    sce <- SingleCellExperiment::SingleCellExperiment(list(counts=counts))
+    libsizes <- colSums(counts)
+    SingleCellExperiment::sizeFactors(sce) <- libsizes/mean(libsizes)
+    
+    ## run selVarGenes
+    sel <- selVarGenes(data = sce)
+    
+    ## tests
+    expect_true(plotSelVarGenes(sel))
+    expect_true(plotSelVarGenes(sel, colByBin=TRUE))
+    expect_error(plotSelVarGenes())
+    expect_error(plotSelVarGenes(mu))
+    expect_error(plotSelVarGenes(list(mu=mu)))
+    
 })
-
-
-
-
-
-
 
 
 

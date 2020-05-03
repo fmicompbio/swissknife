f987cb8284101c7c1cd2253bee000b6b817c382b
mbstadler <stadler.michael@gmail.com>
2020-05-03 15:13:57 +0200

    adjust test to R 4.0's new color palette

--- tests/testthat/test_col2hex.R
+++ tests/testthat/test_col2hex.R
@@ -12,6 +12,6 @@ test_that("col2hex() works properly", {
   expect_identical(col2hex(c("red", "blue")), c("#FF0000FF", "#0000FFFF"))
   expect_identical(col2hex(c("red", "blue"), alpha = 0.7), c("#FF0000B2", "#0000FFB2"))
   expect_identical(col2hex(c("red", "blue"), alpha = 0.7), col2hex(c("red", "blue"), alpha = 178))
-  expect_identical(col2hex(5), "#00FFFFFF")
+  expect_identical(col2hex(5), "#28E2E5FF")
    
 })

fa4d765d8dbe948b8b54c001a8007157b9c2a990
mbstadler <stadler.michael@gmail.com>
2020-12-06 11:58:04 +0100

    increase test coverage

--- tests/testthat/test_cell_labeling.R
+++ tests/testthat/test_cell_labeling.R
@@ -29,11 +29,12 @@ SingleCellExperiment::logcounts(sce) <- log2(sweep(as.matrix(SingleCellExperimen
 
 
 test_that("normGenesetExpression() works properly", {
-        ## these should fail
+    ## these should fail
     expect_error(normGenesetExpression("not_a_sce", NULL))
     expect_error(normGenesetExpression(sce, c("1","2","3")))
     expect_error(normGenesetExpression(sce, c("g1")))
     expect_error(normGenesetExpression(sce, c("not","known","genes")))
+    expect_error(normGenesetExpression(sce, c("g1","g2","g3"), expr_values = FALSE))
     expect_error(normGenesetExpression(sce, c("g1","g2","g3"), R = "error"))
     expect_error(normGenesetExpression(sce, c("g1","g2","g3"), R = 1:3))
     expect_error(normGenesetExpression(sce, c("g1","g2","g3"), R = -1))
@@ -43,6 +44,7 @@ test_that("normGenesetExpression() works properly", {
     expect_error(normGenesetExpression(sce, c("g1","g2","g3"), R = 20, BPPARAM = "error"))
     
     ## run function
+    set.seed(7)
     selgns <- sample(rownames(sce), size = 20)
     res1 <- normGenesetExpression(sce = sce, genes = selgns,
                                   expr_values = "logcounts", R = 40,
@@ -50,13 +52,18 @@ test_that("normGenesetExpression() works properly", {
                                   BPPARAM = BiocParallel::SerialParam())
     res2 <- normGenesetExpression(sce = sce, genes = selgns,
                                   expr_values = 2, R = 40,
+                                  subset.row = seq.int(nrow(sce)),
                                   BPPARAM = BiocParallel::MulticoreParam(workers = 4L))
+    res3 <- normGenesetExpression(sce = sce[unique(c(paste0("g",1:100),selgns))], genes = selgns,
+                                  expr_values = 2, R = 2,
+                                  subset.row = rep(TRUE, 120L))
     
     ## tests
     expect_is(res1, "numeric")
     expect_length(res1, ncol(sce))
     expect_gt(cor(res1, res2), 0.9)
     expect_identical(names(res1), names(res2))
+    expect_identical(names(res1), names(res3))
     expect_identical(names(res1), colnames(sce))
 })
 
@@ -69,6 +76,7 @@ test_that("labelCells() works properly", {
     expect_error(labelCells(sce, list(tA = 1:2, tB = 3)))
     expect_error(labelCells(sce, list(tA = 1:2, tB = 3:4)))
     expect_error(labelCells(sce, list(tA = c("1","2"), tB = c("3","4"))))
+    expect_error(labelCells(sce, markergenes, expr_values = FALSE))
     expect_error(labelCells(sce, markergenes, fraction_topscoring = "low"))
     expect_error(labelCells(sce, markergenes, fraction_topscoring = c(.01, .05)))
     expect_error(labelCells(sce, markergenes, fraction_topscoring = 2.5))

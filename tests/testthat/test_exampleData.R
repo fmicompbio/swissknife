66217890f4e388df1ae881a88bd9dfb8bff25ea8
mbstadler <stadler.michael@gmail.com>
2023-05-24 08:26:43 +0200

    add unit tests for exampleData()

---
+++ tests/testthat/test_exampleData.R
@@ -0,0 +1,52 @@
+test_that("exampleData() works properly", {
+
+    # arguments
+    expect_error(exampleData(FALSE), "name")
+    expect_error(exampleData(c("mycars", "mycars_summary")), "name")
+    expect_error(exampleData("error"), "must be one of")
+    expect_error(exampleData("mycars", "error"), "envir")
+    expect_error(exampleData("mycars", globalenv(), "error"), "verbose")
+
+    # listing
+    expect_output(df0 <- exampleData("list"))
+    expect_is(df0, "data.frame")
+    expect_identical(colnames(df0),
+                     c("name", "created", "description", "is_collection"))
+
+    # return value
+    expect_message(df1 <- exampleData("mycars", verbose = TRUE))
+    expect_is(df1, "data.frame")
+    expect_true(exists("mycars", envir = globalenv()))
+    expect_identical(dim(get("mycars", envir = globalenv())), c(32L, 13L))
+    rm("mycars", envir = globalenv())
+
+    expect_message(df2 <- exampleData("mycars_summary", verbose = TRUE))
+    expect_is(df2, "data.frame")
+    expect_true(exists("mycars_summary", envir = globalenv()))
+    expect_identical(dim(get("mycars_summary", envir = globalenv())), c(3L, 3L))
+    rm("mycars_summary", envir = globalenv())
+
+    df3 <- exampleData("2023-05", verbose = FALSE)
+    expect_is(df3, "data.frame")
+    expect_identical(dim(df3), c(2L, 4L))
+    expect_identical(rbind(df1, df2), df3)
+    expect_true(exists("mycars", envir = globalenv()))
+    expect_true(exists("mycars_summary", envir = globalenv()))
+    rm("mycars", "mycars_summary", envir = globalenv())
+
+    expect_message(df4 <- exampleData("new", verbose = TRUE))
+    expect_is(df4, "data.frame")
+    expect_identical(df3, df4)
+    expect_true(exists("mycars", envir = globalenv()))
+    expect_true(exists("mycars_summary", envir = globalenv()))
+    rm("mycars", "mycars_summary", envir = globalenv())
+
+    # environment
+    myenv <- new.env()
+    df1 <- exampleData("mycars", envir = myenv, verbose = FALSE)
+    expect_true(!exists("mycars", envir = globalenv()))
+    expect_true(exists("mycars", envir = myenv))
+    expect_identical(dim(get("mycars", envir = myenv)), c(32L, 13L))
+    rm("mycars", envir = myenv)
+    rm(myenv)
+})

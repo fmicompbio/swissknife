9951bd4ad1d08b3199a33beded23655535b93cb7
Fiona713 <fonis@hotmail.de>
2024-02-09 14:41:12 +0100

    add column groups to weighted Mean function and remove pseudocount

--- R/weightedMeanByID.R
+++ R/weightedMeanByID.R
@@ -1,62 +1,79 @@
 #' @title Aggregate different rows assigned to the same ID by calculating a 
 #' weighted mean 
 #'
-#' @description After calculating row means to summarize across replicates, 
-#' values of different rows that are assigned to the same ID are summarized by 
-#' calculating a weighted mean. The weights are the values themselves.
+#' @description First row means are calculated to summarize across replicates
+#' by a group variable. Then different row means that are assigned to the same 
+#' feature ID are summarized by calculating a weighted mean. This weighted mean
+#' is the sum of the squared row means divided by the sum of the row means.
 #' 
 #' @author Fiona Ross
 #' 
-#' @param SE \code{SummarizedExperiment} object that contains an assay with 
-#' the values to be aggregated and a rowData column with IDs to indicate
-#' which values of different rows to combine. 
-#'   
-#' @param assay \code{character} name of the assay in the SummarizedExperiment
-#' object to use.
-#' 
-#' @param idCol \code{character} column name in the rowData of the
-#' SummarizedExperiment indicating the ID assigned to each row.
-#' 
-#' @param log2Transformed \code{logical} indicating whether the data is log2
-#' transformed. 
-#' 
-#' @param pseudocount \code{numeric} pseudocount to add to avoid dividing by 0.
+#' @param SE a \code{SummarizedExperiment} object that contains an assay with
+#' values to be aggregated and a rowData column with IDs to indicate which 
+#' rows to combine. 
+#' @param assay the name of the assay in the SummarizedExperiment object that 
+#' should be aggregated.
+#' @param idCol the column name in the rowData of the SummarizedExperiment 
+#' indicating the ID assigned to each row.
+#' @param groupCol the column name in the colData of the SummarizedExperiment 
+#' indicating which columns should be averaged as replicates, before the
+#' weighted mean is calculated across rows.
+#' @param log2Transformed a \code{logical} indicating whether values in the 
+#' assay are log2 transformed. If log2Transformed is TRUE, an exponential
+#' transformation will be applied before aggregating the values and another log 
+#' transformation afterwards. 
 #' 
 #' @return \code{data.frame} with one column that is a numeric vector of the
 #' weighted means per unique ID. If log2Transformed is true the output will be 
 #' log2 transformed again. The rownames of the dataframe correspond to the 
 #' unique IDs.
 #' 
+#' @examples
+#' set.seed(123)
+#' meansRows <- sample(1:100, 10, replace = TRUE)
+#' dat <- unlist(lapply(meansRows, function(m) {
+#'     rnorm(n = 5, mean = m, sd = 0.1*m)
+#' }))
+#' ma <- matrix(dat, nrow = 10, ncol = 5, byrow = TRUE)
+#' IDs <- data.frame(ID = sample(c("A", "B", "C", "D"), size = 10, replace = TRUE))
+#' Groups <- data.frame(group = c("Y","Y", "Z", "Z", "Z"))
+#' mockSE <- SummarizedExperiment(assays = list(promoterenr = ma),
+#'                               rowData = IDs,
+#'                               colData = Groups)
+#' weightedMeanByID(mockSE, "counts", idCol = "ID", log2Transformed = FALSE)                                
+#' 
 #' @importFrom stats aggregate weighted.mean
 #' @importFrom SummarizedExperiment assay rowData
 #' @importFrom dplyr mutate across where
- 
- 
-weightedMeanByID <- function(SE, assay, idCol = "GENEID", log2Transformed = TRUE, pseudocount = 0.01) {
+#' @export
+weightedMeanByID <- function(SE, assay, idCol = "GENEID", groupCol = "group", log2Transformed = TRUE) {
     .assertPackagesAvailable("stats")
     if (!is(SE, "SummarizedExperiment")) {
-        stop("Provide data to aggregate in a Summarized Experiment")
+        stop("Provide data to aggregate in a SummarizedExperiment")
     }
     if (!is.character(assay)) {
-        stop("Provide the name of the Summarized Experiment assay to use for weighted mean calculation as a character")
+        stop("Provide the assay name as a character")
     }
     if (!is.character(idCol)) {
-        stop("Provide the name of the ID column over which the weighted mean is calculated as a character")
+        stop("Provide the name of the rowData column as a character")
+    }
+    if (!is.character(groupCol)) {
+        stop("Provide the name of the colData column as a character")
     }
     if (!is.logical(log2Transformed)) {
-        stop("log2Transformed can only be TRUE or FALSE to indicate whether the data is log2 transformed")
+        stop("log2Transformed must be a logical indicating if data is log2 transformed")
     }
     if (!idCol %in% colnames(SummarizedExperiment::rowData(SE))) {
-        stop("The ID column must be an existing columnname in the rowData of the Summarized Experiment object")
+        stop("idCol must be an existing column name in the rowData of the SummarizedExperiment object")
+    }
+    if (!groupCol %in% colnames(SummarizedExperiment::colData(SE))) {
+        stop("groupCol must be an existing column name in the colData of the SummarizedExperiment object")
     }
     if (!assay %in% names(SummarizedExperiment::assays(SE))) {
-        stop("The assay name must be an existing assay of the Summarized Experiment object")
-    }
-    if (!is.numeric(pseudocount)) {
-        stop("Provide the pseudocount as a numeric")
+        stop("The assay name must be an existing assay of the SummarizedExperiment object")
     }
     if (any(is.na(SummarizedExperiment::assay(SE)))) {
-        warning("There are NAs in the assay to aggregate. rowMeans will be calculated over remaining values and used for the weighted mean.")
+        warning("The assay contains NAs, remaining values will be aggregated.")
     }
     
     if (log2Transformed) {
@@ -65,22 +82,40 @@ weightedMeanByID <- function(SE, assay, idCol = "GENEID", log2Transformed = TRUE
         ma <- SummarizedExperiment::assay(SE, assay)
     }
     
-    promoterenr <- as.data.frame(rowMeans(ma, na.rm = TRUE))
-    promoterenr$gene_id <- SummarizedExperiment::rowData(SE)[,idCol]
-    
-    weightedMeanByID <- stats::aggregate(
-        . ~ gene_id,
-        data = promoterenr,
-        FUN = function(x)stats::weighted.mean(x + pseudocount, x + pseudocount))
-    
-    if (log2Transformed) {
-        weightedMeanByID <- weightedMeanByID %>%
-            dplyr::mutate(across(where(is.numeric), log2))
-    }
-    
-    rownames(weightedMeanByID) <- weightedMeanByID$gene_id
-    weightedMeanByID$gene_id <- NULL
-    colnames(weightedMeanByID) <- c("weightedMean")
-    
+    weightedMeanByID <- do.call(cbind, lapply(unique(SummarizedExperiment::colData(SE)[, groupCol]), function(g) {
+        sel <- colData(SE)[, groupCol] == g
+        ma <- ma[, sel, drop = FALSE]
+        promoterenr <- as.data.frame(rowMeans(ma, na.rm = TRUE))
+        promoterenr$gene_id <- SummarizedExperiment::rowData(SE)[, idCol]
+        
+        weightedMeanByID <- stats::aggregate(
+            . ~ gene_id,
+            data = promoterenr,
+            FUN = function(x) {
+                if (sum(x) != 0) {
+                    stats::weighted.mean(x, x)
+                } else if (sum(x) == 0) {
+                    0
+                }
+            })
+        
+        if (log2Transformed) {
+            weightedMeanByID <- weightedMeanByID %>%
+                dplyr::mutate(across(where(is.numeric), log2))
+        }
+        
+        rownames(weightedMeanByID) <- weightedMeanByID$gene_id
+        weightedMeanByID$gene_id <- NULL
+        colnames(weightedMeanByID) <- c("weightedMean")
+        
+        if (any(!unique(promoterenr$gene_id) %in% rownames(weightedMeanByID))) {
+            append <- unique(promoterenr$gene_id)[!unique(promoterenr$gene_id) %in% rownames(weightedMeanByID)]
+            append <- data.frame(weightedMean = rep(NA, length(append)), row.names = append)
+            weightedMeanByID <- rbind(weightedMeanByID, append)
+        }
+        
+        return(weightedMeanByID)
+    }))
+    colnames(weightedMeanByID) <- unique(SummarizedExperiment::colData(SE)[, groupCol])
     return(weightedMeanByID)
 }
--- tests/testthat/test_weightedMeanByID.R
+++ tests/testthat/test_weightedMeanByID.R
@@ -3,47 +3,72 @@ library(SummarizedExperiment)
 set.seed(123)
 meansRows <- sample(1:100, 10, replace = TRUE)
 dat <- unlist(lapply(meansRows, function(m) {
-    rnorm(n = 3, mean = m, sd = 0.1*m)
+    rnorm(n = 5, mean = m, sd = 0.1*m)
 }))
-ma <- matrix(dat, nrow = 10, ncol = 3, byrow = TRUE)
+ma <- matrix(dat, nrow = 10, ncol = 5, byrow = TRUE)
 IDs <- data.frame(ID = sample(c("A", "B", "C", "D"), size = 10, replace = TRUE))
+Groups <- data.frame(group = c("Y","Y", "Z", "Z", "Z"))
 
 mockSE <- SummarizedExperiment(assays = list(promoterenr = ma),
-                     rowData = IDs)
+                     rowData = IDs,
+                     colData = Groups)
 
 
 test_that("weightedMeanByID only takes appropriate input", {
-    expect_error(weightedMeanByID(SummarizedExperiment::assay(mockSE), "promoterenr", idCol = "ID", log2Transformed = FALSE), "Provide data to aggregate in a Summarized Experiment")
-    expect_error(weightedMeanByID(mockSE, "counts", idCol = "ID", log2Transformed = FALSE), "The assay name must be an existing assay of the Summarized Experiment object")
-    expect_error(weightedMeanByID(mockSE, ma, idCol = "ID", log2Transformed = FALSE), "Provide the name of the Summarized Experiment assay to use for weighted mean calculation as a character")
-    expect_error(weightedMeanByID(mockSE, "promoterenr", log2Transformed = FALSE), "The ID column must be an existing columnname in the rowData of the Summarized Experiment object")
-    expect_error(weightedMeanByID(mockSE, "promoterenr", idCol = IDs, log2Transformed = FALSE), "Provide the name of the ID column over which the weighted mean is calculated as a character")
-    expect_error(weightedMeanByID(mockSE, "promoterenr", idCol = "ID", log2Transformed = "log10"), "log2Transformed can only be TRUE or FALSE to indicate whether the data is log2 transformed")
-    expect_error(weightedMeanByID(mockSE, "promoterenr", idCol = "ID", log2Transformed = TRUE, pseudocount = TRUE), "Provide the pseudocount as a numeric")
+    expect_error(weightedMeanByID(SummarizedExperiment::assay(mockSE), "promoterenr", idCol = "ID", log2Transformed = FALSE), "Provide data to aggregate in a SummarizedExperiment")
+    expect_error(weightedMeanByID(mockSE, "counts", idCol = "ID", log2Transformed = FALSE), "The assay name must be an existing assay of the SummarizedExperiment object")
+    expect_error(weightedMeanByID(mockSE, ma, idCol = "ID", log2Transformed = FALSE), "Provide the assay name as a character")
+    expect_error(weightedMeanByID(mockSE, "promoterenr", log2Transformed = FALSE), "idCol must be an existing column name in the rowData of the SummarizedExperiment object")
+    expect_error(weightedMeanByID(mockSE, "promoterenr", idCol = IDs, log2Transformed = FALSE), "Provide the name of the rowData column as a character")
+    expect_error(weightedMeanByID(mockSE, "promoterenr", idCol = "ID", log2Transformed = "log10"), "log2Transformed must be a logical indicating if data is log2 transformed")
+    expect_error(weightedMeanByID(mockSE, "promoterenr", idCol = "ID", groupCol = Groups, log2Transformed = FALSE), "Provide the name of the colData column as a character")
+    expect_error(weightedMeanByID(mockSE, "promoterenr", idCol = "ID", groupCol = "fantastic", log2Transformed = FALSE), "groupCol must be an existing column name in the colData of the SummarizedExperiment object")
     
     alt_SE <- mockSE
     assay(alt_SE)[c(4,8)] <- NA
-    expect_warning(weightedMeanByID(alt_SE, "promoterenr", idCol = "ID", log2Transformed = TRUE), "There are NAs in the assay to aggregate. rowMeans will be calculated over remaining values and used for the weighted mean.")
+    expect_warning(weightedMeanByID(alt_SE, "promoterenr", idCol = "ID", log2Transformed = FALSE), "The assay contains NAs, remaining values will be aggregated.")
 })
 
 
 test_that("weightedMeanByID works properly", {
     res <- weightedMeanByID(mockSE, "promoterenr", idCol = "ID", log2Transformed = FALSE)
     expect_identical(length(unique(IDs$ID)), length(rownames(res)))
+    expect_true(all(unique(IDs$ID) %in% rownames(res)))
+    expect_true(all(unique(Groups$group) %in% colnames(res)))
     
-    lapply(unique(IDs$ID), function(l) {
-        sel <- which(rowData(mockSE)$ID == l)
-        means <- lapply(sel, function(s) {
-            sum(assay(mockSE)[s,])/3 + 0.01
+    lapply(list("Y", "Z"), function(gc) {
+        lapply(unique(IDs$ID), function(l) {
+            sel <- which(rowData(mockSE)$ID == l)
+            means <- lapply(sel, function(s) {
+                sum(assay(mockSE)[s, colData(mockSE)$group == gc])/sum(colData(mockSE)$group == gc)
+            })
+            squares <- lapply(means, function(m) {
+                m^2
+            })
+            expect_equal(res[l,gc], sum(unlist(squares))/sum(unlist(means)))
         })
-        squares <- lapply(means, function(m) {
-            m^2
-        })
-        expect_identical(res[l,], sum(unlist(squares))/sum(unlist(means)))
     })
-    expect_equal(ncol(res), 1)
+    
+    expect_equal(ncol(res), length(unique(colData(mockSE)$group)))
+    
     loggedSE <- mockSE
     assay(loggedSE) <- log2(assay(loggedSE))
     expect_equal(weightedMeanByID(loggedSE, "promoterenr", idCol = "ID", log2Transformed = TRUE), log2(res))
     expect_is(res, "data.frame")
+    
+    # test that result is NA if all input values are NA
+    na_SE <- mockSE
+    assay(na_SE)[rowData(na_SE)$ID == "A",] <- NA
+    res_na <- weightedMeanByID(na_SE, "promoterenr", idCol = "ID", log2Transformed = FALSE)
+    expect_true(all(is.na(res_na["A",])))
+    
+    # test that result is 0 if all input values are 0
+    assay(na_SE)[rowData(na_SE)$ID == "A",] <- 0
+    res_null <- weightedMeanByID(na_SE, "promoterenr", idCol = "ID", log2Transformed = FALSE)
+    expect_true(all(res_null["A",] == 0))
+    
+    # test that this still works if we have only one group
+    oneSE <- mockSE[, colData(mockSE)$group == "Y"]
+    res_one <- weightedMeanByID(onereSE, "promoterenr", idCol = "ID", log2Transformed = FALSE)
+    expect_identical(res$Y, res_one$Y)
 })

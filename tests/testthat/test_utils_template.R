9a36d481536662e1ae6d8b76a415fe20e28521ee
Charlotte Soneson <charlottesoneson@gmail.com>
2025-04-27 09:33:27 +0200

    Load cli before running the utils template tests

--- tests/testthat/test_utils_template.R
+++ tests/testthat/test_utils_template.R
@@ -1,3 +1,6 @@
+suppressPackageStartupMessages({
+    library(cli)
+})
 test_that("utils template works properly", {
     source(system.file("templates", "utils_template.R", package = "swissknife"))
     source(system.file("templates", "tests_utils_template.R",
